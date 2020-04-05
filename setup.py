b8a4494e78feafed30f1e05f5bf8e5f47d610b7b
Giannis Daras <daras.giannhs@gmail.com>
2020-04-05 13:17:14 +0300

    Add setup for packaging

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pcl",
+    version="0.0.1",
+    author="Giannis Daras (@giannisdaras), Marios Papachristou (@papachristoumarios)",
+    author_email="daras.giannhs@gmail.com, papachristoumarios@gmail.com",
+    description="Compiler for pcl language",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/papachristoumarios/pcl",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+    ],
+    python_requires='>=3.6',
+)
