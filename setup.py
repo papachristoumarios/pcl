bed20dbb2d699c60ced0277b4ea940b454478082
Marios Papachristou <papachristoumarios@gmail.com>
2020-04-05 14:25:28 +0300

    Add basic structure
     1. pclc.py is compiler driver
     2. Add package structure
    
    Co-authored-by: Giannis Daras <daras.giannhs@gmail.com>
    Co-authored-by: Marios Papachristou <papachristoumarios@gmail.com>

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/papachristoumarios/pcl",
     packages=setuptools.find_packages(),
+    scripts=['pclc.py']
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
