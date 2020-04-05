ccb78e698ea230888e4e8a1610926274372da886
Giannis Daras <daras.giannhs@gmail.com>
2020-04-05 18:34:27 +0300

    Initialize parser
    Co-authored-by: Marios Papachristou <papachristoumarios@gmail.com>
    Co-authored-by: Giannis Daras <daras.giannhs@gmail.com>

---
+++ pcl/parser.py
@@ -0,0 +1,40 @@
+from sly import Parser
+from pcl.lexer import PCLLexer
+from pcl.error import PCLParserError
+from pcl.ast import *
+
+class PCLParser(Parser):
+    '''
+        Contains the main functionality of the PCL Parser
+        built with the SLY tool
+    '''
+
+    # Tokens from PCLLexer
+    tokens = PCLLexer.tokens
+
+    def __init__(self, builder=None, module=None, printf=None):
+        '''
+            Constructor for the PCL Parser
+
+            Args:
+                builder: LLVM IR code builder through codegen. Defaults to None
+                module: LLVM module object
+                printf: LLVM printf function
+        '''
+        self.builder = builder
+        self.module = module
+        self.printf = printf
+
+
+
+
+    def error(self, p):
+        msg = 'Illegal rule {}'.format(p)
+        raise PCLParserError(msg)
+
+
+
+
+
+
+if __name__ == '__main__':
