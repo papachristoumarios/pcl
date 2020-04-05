4d92e14b94c46acfad2954a84e3797e73d2c2c26
Giannis Daras <daras.giannhs@gmail.com>
2020-04-05 18:35:03 +0300

    WIP: Add regexes to parse tokens
    Co-authored-by: Marios Papachristou <papachristoumarios@gmail.com>
    Co-authored-by: Giannis Daras <daras.giannhs@gmail.com>

---
+++ pcl/lexer.py
@@ -0,0 +1,173 @@
+from sly import Lexer
+from pcl.error import PCLLexerError
+import re
+
+def regex(s):
+    return re.escape(s)
+
+
+class PCLLexer(Lexer):
+    tokens = {
+        # keywords
+        AND,
+        DO,
+        IF,
+        OF,
+        THEN,
+        ARRAY,
+        ELSE,
+        INTEGER,
+        OR,
+        TRUE,
+        BEGIN,
+        END,
+        LABEL,
+        PROCEDURE,
+        VAR,
+        BOOLEAN,
+        FALSE,
+        MOD,
+        PROGRAM,
+        WHILE,
+        CHAR,
+        FORWARD,
+        NEW,
+        REAL,
+        DISPOSE,
+        FUNCTION,
+        NIL,
+        RESULT,
+        DIV,
+        GOTO,
+        NOT,
+        RETURN,
+        # Arithmetic operators
+        EQUAL,
+        LPAREN,
+        RPAREN,
+        PLUS,
+        MINUS,
+        TIMES,
+        FRAC,
+        GT,
+        LT,
+        GTE,
+        LTE,
+        LSQUARE,
+        RSQUARE,
+        EXP,
+        PTR,
+        NEG,
+        # Separators
+        SET,
+        SEMICOLON,
+        COLON,
+        DCOLON,
+        COMMA,
+        # others
+        DIGIT,
+        INT_CONS,
+        REAL_CONS,
+        CHARACTER,
+        NAME,
+        COMMENT,
+        STRING,
+        NEWLINE
+
+    }
+
+    # keywords
+    AND = regex('and')
+    DO = regex('do')
+    IF = regex('if')
+    OF = regex('of')
+    THEN = regex('then')
+    ARRAY = regex('array')
+    ELSE = regex('else')
+    INTEGER = regex('integer')
+    OR = regex('or')
+    TRUE = regex('true')
+    BEGIN = regex('begin')
+    END = regex('end')
+    LABEL = regex('label')
+    PROCEDURE = regex('procedure')
+    VAR = regex('var')
+    BOOLEAN = regex('boolean')
+    FALSE = regex('false')
+    MOD = regex('mod')
+    PROGRAM = regex('program')
+    WHILE = regex('while')
+    CHAR = regex('char')
+    FORWARD = regex('forward')
+    NEW = regex('new')
+    REAL = regex('real')
+    DISPOSE = regex('dispose')
+    FUNCTION = regex('function')
+    NIL = regex('nil')
+    RESULT = regex('result')
+    DIV = regex('div')
+    GOTO = regex('goto')
+    NOT = regex('not')
+    RETURN = regex('return')
+
+    # Arithmetic operators
+    EQUAL = regex('=')
+    LPAREN = regex('(')
+    RPAREN = regex(')')
+    PLUS = regex('+')
+    MINUS = regex('-')
+    TIMES = regex('*')
+    FRAC = regex('/')
+    GT = regex('>')
+    LT = regex('<')
+    GTE = regex('>=')
+    LTE = regex('<=')
+    LSQUARE = regex('[')
+    RSQUARE = regex(']')
+    EXP = regex('^')
+    PTR = regex('@')
+    NEG = regex('<>')
+
+    # Separators
+    SET = regex(':=')
+    SEMICOLON = regex(';')
+    COLON = regex('.')
+    DCOLON = regex(':')
+    COMMA = regex(',')
+
+    # "Special" tokens:
+    NAME = r"(" + r"[a-z]" + r'|' + r"[A-Z]" + r")" + r"(" + r"[a-z]" + r'|' + r"[A-Z]" + r"|[0-9]" + r"|" + r"_)*"
+    INT_CONS = r'[0-9]+'
+    REAL_CONS = r"[0-9]+(\.[0-9]+(['E', 'e']['\+','\-']?[0-9]+)?)?"
+    CHARACTER = "'" + ".|" + "'"
+    STRING = r"\"[^\"]*\""
+
+    COMMENT = r'(?s)\(\*.*?\*\)'
+    ignore = r'[\t, \r, \s]+'
+    ignore_newline = '\n+'
+
+    # "Special" functions:
+    def COMMENT(self, t):
+        self.lineno += t.value.count('\n')
+
+    def ignore_newline(self, t):
+        self.lineno += t.value.count('\n')
+
+    def error(self, t):
+        msg = 'Illegal character {} at line {}'.format(t.value[0], self.lineno)
+        raise PCLLexerError(msg)
+
+
+if __name__ == '__main__':
+    lexer = PCLLexer()
+    s = '''
+        program hello;
+        begin
+        writeString("hello")
+        end.
+        '''
+    token_names = [x.value for x in lexer.tokenize(s)]
+    token_types = [x.type for x in lexer.tokenize(s)]
+
+    print(token_names)
+    print(token_types)
