f2d9c1427cf3a587f1e5c8783374f8ed8e50f00a
Marios Papachristou <papachristoumarios@gmail.com>
2020-04-11 19:22:11 +0300

    Add positive tests
    Co-authored-by: Marios Papachristou <papachristoumarios@gmail.com>
    Co-authored-by: Giannis Daras <daras.giannhs@gmail.com>

--- tests/semantic_analyzer.py
+++ tests/semantic_analyzer.py
@@ -25,29 +25,27 @@ for filename in glob.iglob(os.path.join(neg_examples_folder, "*.pcl")):
 
 
 
-
-# def test_valid():
-#     for example in valid_examples:
-#         logging.log(logging.INFO, 'Running example: {}'.format(example))
-#         tokens = lexer.tokenize(example)
-#         program = parser.parse(tokens)
-#         program.sem()
-
-@pytest.mark.parametrize("example", invalid_examples)
-def test_invalid(example):
+@pytest.mark.parametrize("example", valid_examples)
+def test_valid(example):
     if example == '':
         return
     logging.log(logging.INFO, 'Running example: {}'.format(example))
-    try:
-        tokens = lexer.tokenize(example)
-        program = parser.parse(tokens)
-        program.sem()
-        assert 1 == 0, 'Negative program passed'
-    except PCLSemError as e:
-        logging.log(logging.DEBUG, e)
-    except PCLParserError as e:
-        logging.log(logging.CRITICAL, e)
-        assert 1 == 0, "Parser error"
+    tokens = lexer.tokenize(example)
+    program = parser.parse(tokens)
+    program.sem()
+
+# @pytest.mark.parametrize("example", invalid_examples)
+# def test_invalid(example):
+#     if example == '':
+#         return
+#     logging.log(logging.INFO, 'Running example: {}'.format(example))
+#     try:
+#         tokens = lexer.tokenize(example)
+#         program = parser.parse(tokens)
+#         program.sem()
+#         assert 1 == 0, 'Negative program passed'
+#     except PCLError as e:
+#         print(e)
 
 if __name__ == '__main__':
     pytest.main(args=[os.path.abspath(__file__)])
