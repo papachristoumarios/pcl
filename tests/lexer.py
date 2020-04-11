136def1c674206d798308195bd96dbc7bafa47a6
Giannis Daras <daras.giannhs@gmail.com>
2020-04-11 15:37:37 +0300

    Fix lexer test
    Co-authored-by: Marios Papachristou <papachristoumarios@gmail.com>
    Co-authored-by: Giannis Daras <daras.giannhs@gmail.com>

--- tests/lexer.py
+++ tests/lexer.py
@@ -1,5 +1,8 @@
 import pytest
-from pcl.pcl.lexer import PCLLexer as Lexer
+from pcl import PCLLexer as Lexer
+import pytest
+import os
+
 
 lexer = Lexer()
 
@@ -36,3 +39,73 @@ def test_collatz():
 
     end.
     '''
+    assert(tokenize(s) == ['PROGRAM', 'NAME', 'SEMICOLON', 'LABEL', 'NAME',
+                           'COMMA', 'NAME', 'SEMICOLON', 'FORWARD',
+                           'PROCEDURE', 'NAME', 'LPAREN', 'RPAREN',
+                           'SEMICOLON', 'VAR', 'NAME', 'DCOLON', 'INTEGER',
+                           'SEMICOLON', 'BEGIN', 'NAME', 'SET', 'INT_CONS',
+                           'SEMICOLON', 'WHILE', 'NAME', 'GT', 'INT_CONS',
+                           'DO', 'BEGIN', 'NAME', 'LPAREN', 'NAME', 'RPAREN',
+                           'SEMICOLON', 'IF', 'NAME', 'MOD', 'INT_CONS',
+                           'EQUAL', 'INT_CONS', 'THEN', 'NAME', 'SET', 'NAME',
+                           'DIV', 'INT_CONS', 'ELSE', 'NAME', 'SET',
+                           'INT_CONS', 'TIMES', 'NAME', 'PLUS', 'INT_CONS',
+                           'SEMICOLON', 'END', 'SEMICOLON', 'END', 'COLON'])
+
+def test_reverse():
+    s = '''
+        program reverse;
+            function strlen (var s : array of char) : integer;
+            begin
+                result := 0;
+                while s[result] <> 'c' do result := result + 1
+            end;
+
+            var r : array [32] of char;
+            procedure reverse (var s : array of char);
+            var i, l : integer;
+            begin
+                l := strlen(s);
+                i := 0;
+                while i < l do
+                    begin
+                        r[i] := s[l-i-1];
+                        i := i+1
+                    end;
+                r[i] := "\0"
+            end;
+
+            begin
+                reverse("\n!dlrow olleH");
+                writeString(r)
+            end.
+    '''
+    assert(tokenize(s) == ['PROGRAM', 'NAME', 'SEMICOLON', 'FUNCTION', 'NAME',
+                           'LPAREN', 'VAR', 'NAME', 'DCOLON', 'ARRAY', 'OF',
+                           'CHAR', 'RPAREN', 'DCOLON', 'INTEGER', 'SEMICOLON',
+                           'BEGIN', 'RESULT', 'SET', 'INT_CONS', 'SEMICOLON',
+                           'WHILE', 'NAME', 'LSQUARE', 'RESULT', 'RSQUARE',
+                           'NEG', 'CHARACTER', 'DO', 'RESULT',
+                           'SET', 'RESULT', 'PLUS', 'INT_CONS', 'END',
+                           'SEMICOLON', 'VAR', 'NAME', 'DCOLON', 'ARRAY',
+                           'LSQUARE', 'INT_CONS', 'RSQUARE', 'OF', 'CHAR',
+                           'SEMICOLON', 'PROCEDURE', 'NAME', 'LPAREN', 'VAR',
+                           'NAME', 'DCOLON', 'ARRAY', 'OF', 'CHAR', 'RPAREN',
+                           'SEMICOLON', 'VAR', 'NAME', 'COMMA', 'NAME',
+                           'DCOLON', 'INTEGER', 'SEMICOLON', 'BEGIN', 'NAME',
+                           'SET', 'NAME', 'LPAREN', 'NAME', 'RPAREN',
+                           'SEMICOLON', 'NAME', 'SET', 'INT_CONS', 'SEMICOLON',
+                           'WHILE', 'NAME', 'LT', 'NAME', 'DO', 'BEGIN',
+                           'NAME', 'LSQUARE', 'NAME', 'RSQUARE', 'SET',
+                           'NAME', 'LSQUARE', 'NAME', 'MINUS', 'NAME',
+                           'MINUS', 'INT_CONS', 'RSQUARE', 'SEMICOLON', 'NAME',
+                           'SET', 'NAME', 'PLUS', 'INT_CONS', 'END',
+                           'SEMICOLON', 'NAME', 'LSQUARE', 'NAME', 'RSQUARE',
+                           'SET', 'STRING', 'END', 'SEMICOLON', 'BEGIN',
+                           'NAME', 'LPAREN', 'STRING', 'RPAREN', 'SEMICOLON',
+                           'NAME', 'LPAREN', 'NAME', 'RPAREN', 'END', 'COLON'])
+
+
+
+if __name__ == '__main__':
+    pytest.main(args=[os.path.abspath(__file__)])
