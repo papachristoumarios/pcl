352c4510072e4da0e4527b9fd30351dc4135af72
Marios Papachristou <papachristoumarios@gmail.com>
2020-04-14 22:11:27 +0300

    Various changes
    1. Fix PEP8
    2. Remove _declare_printf(), declare_scanf()

--- pcl/codegen.py
+++ pcl/codegen.py
@@ -3,18 +3,21 @@ from llvmlite import ir, binding
 from pcl.error import PCLCodegenError
 import os
 
+
 class LLVMTypeSize:
     T_BOOL = 1
     T_CHAR = 1
     T_REAL = 10
     T_PTR = 2
-    T_INT = 8
+    T_INT = 32
 
     @staticmethod
     def get_const_array_size(n, t):
         if n <= 0:
-            raise PCLCodegenError('Allocating array with non-positive number of elements')
+            raise PCLCodegenError(
+                'Allocating array with non-positive number of elements')
         return n * t.value
+
 
 class LLVMTypes:
     T_INT = ir.IntType(LLVMTypeSize.T_INT)
@@ -23,24 +26,28 @@ class LLVMTypes:
     T_PROC = ir.VoidType()
     T_PTR = ir.PointerType(LLVMTypeSize.T_PTR)
     T_REAL = ir.DoubleType()
+    T_NIL = ir.IntType(1).as_pointer()
 
     mapping = {
-        'integer' : T_INT,
-        'bool' : T_BOOL,
-        'real' : T_REAL,
-        'char' : T_CHAR
+        'integer': T_INT,
+        'boolean': T_BOOL,
+        'real': T_REAL,
+        'char': T_CHAR
     }
+
 
 class LLVMConstants:
     ZERO_INT = ir.Constant(LLVMTypes.T_INT, 0)
     ZERO_REAL = ir.Constant(LLVMTypes.T_REAL, 0)
+    NIL = ir.Constant(LLVMTypes.T_NIL, 0)
+
 
 class LLVMOperators:
 
     # Accessible with comp_mapping.get(x, x)
     comp_mapping = {
-        '=' : '==',
-        '<>' : '!=',
+        '=': '==',
+        '<>': '!=',
     }
 
     @staticmethod
@@ -57,11 +64,9 @@ class PCLCodegen:
         self.binding.initialize_native_asmprinter()
         self._config_llvm()
         self._create_execution_engine()
-        self._declare_printf()
-        self._declare_scanf()
 
     def _config_llvm(self):
-        self.module = ir.Module(name='example')
+        self.module = ir.Module()
         self.module.triple = self.binding.get_default_triple()
         func_type = ir.FunctionType(ir.VoidType(), [], False)
         base_func = ir.Function(self.module, func_type, name="main")
@@ -80,16 +85,6 @@ class PCLCodegen:
         backing_mod = binding.parse_assembly("")
         engine = binding.create_mcjit_compiler(backing_mod, target_machine)
         self.engine = engine
-
-    def _declare_printf(self):
-        voidptr_ty = ir.IntType(8).as_pointer()
-        printf_ty = ir.FunctionType(ir.IntType(32), [voidptr_ty], var_arg=True)
-        self.printf = ir.Function(self.module, printf_ty, name="printf")
-
-    def _declare_scanf(self):
-        voidptr_ty = ir.IntType(8).as_pointer()
-        scanf_ty = ir.FunctionType(ir.IntType(32), [voidptr_ty], var_arg=True)
-        self.scanf = ir.Function(self.module, scanf_ty, name="scanf")
 
     def _compile_ir(self):
         """
@@ -118,4 +113,4 @@ class PCLCodegen:
             f.write(final_code)
 
         os.system('llc-8 -filetype=obj {}'.format(llvm_filename))
-        os.system('gcc {} -o {}'.format(obj_filename, output_filename))
+        os.system('gcc {} builtins.c -o {}'.format(obj_filename, output_filename))
