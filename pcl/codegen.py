68cb4f8679f13689810adf759dc7bc6b45abeb61
Marios Papachristou <papachristoumarios@gmail.com>
2020-04-15 17:38:22 +0300

    Tidy codegen module

--- pcl/codegen.py
+++ pcl/codegen.py
@@ -59,35 +59,35 @@ class LLVMOperators:
 class PCLCodegen:
 
     def __init__(self):
+        ''' Initialize  llvm '''
         self.binding = binding
         self.binding.initialize()
         self.binding.initialize_native_target()
         self.binding.initialize_native_asmprinter()
-        self._config_llvm()
 
-    def _config_llvm(self):
+        # Declare module
         self.module = ir.Module()
         self.module.triple = self.binding.get_default_triple()
+
+        # Declare main function
         func_type = ir.FunctionType(ir.VoidType(), [], False)
-        base_func = ir.Function(self.module, func_type, name="main")
-        block = base_func.append_basic_block(name="entry")
+        base_func = ir.Function(self.module, func_type, name='main')
+        block = base_func.append_basic_block()
+
+        # Declare builder
         self.builder = ir.IRBuilder(block)
 
-
-    def compile_ir(self):
-        """
-        Compile the LLVM IR string with the given engine.
-        The compiled module object is returned.
-        """
-        # Create a LLVM module object from the IR
+    def postprocess_module(self, level=2):
+        ''' Module post-processing '''
         self.builder.ret_void()
-        llvm_ir = str(self.module)
-        mod = self.binding.parse_assembly(llvm_ir)
-        mod.verify()
-        self.module = mod
-        self.optimize_module()
 
-        return self.module
+        # Verify module
+        self.module = self.binding.parse_assembly(str(self.module))
+        self.module.verify()
+
+        # Optimize module
+        self.optimize_module(level=level)
+
 
     def optimize_module(self, level=2):
         if level == 0:
@@ -95,22 +95,28 @@ class PCLCodegen:
         elif level < 0 or level >= 3:
             msg = 'Undefined optimization level: {}'.format(msg)
             raise PCLCodegenError(msg)
+
+        # Initialize pass manager builder
         self.pmb = binding.PassManagerBuilder()
+
+        # Declare optimization level
         self.pmb.opt_level = level
 
+        # Configure module pass manager
         self.mpm = binding.ModulePassManager()
         self.pmb.populate(self.mpm)
+
+        # Run optimizations
         self.mpm.run(self.module)
 
     def generate_outputs(self, filename):
-        self.compile_ir()
-        final_code = str(self.module)
+        self.postprocess_module()
         llvm_filename = filename + '.ll'
         obj_filename = filename + '.o'
         output_filename = filename + '.out'
 
         with open(llvm_filename, 'w+') as f:
-            f.write(final_code)
+            f.write(str(self.module))
 
         os.system('llc-8 -filetype=obj {}'.format(llvm_filename))
         os.system('gcc {} builtins.c -lm -o {}'.format(obj_filename, output_filename))
