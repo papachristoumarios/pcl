e9beee91296d41d66f453a128072f2e7d9d7ad03
Giannis Daras <daras.giannhs@gmail.com>
2020-06-16 00:22:54 +0300

    Extern functions

--- pcl/builtins.h
+++ pcl/builtins.h
@@ -1,15 +1,18 @@
 #ifndef builtins_h_
 #define builtins_h_
 
-extern chr* writeInteger(integer);
-extern chr* writeChar(chr);
-extern chr* writeReal(real);
-extern chr* writeBoolean(boolean);
-extern chr* writeString(chr *);
+extern character* writeInteger(integer);
+extern character* writeChar(character);
+extern character* writeReal(real);
+extern character* writeBoolean(boolean);
+extern character* writeString(character *);
 extern integer readInteger();
 extern boolean readBoolean();
-extern chr readChar();
+extern character readChar();
 extern real readReal();
-extern chr* readString(integer, chr*)
-
+extern character* readString(integer, character*)
+extern integer trunc2(real);
+extern integer round2(real);
+extern character chr(integer);
+extern integer ord(character);
 #endif
